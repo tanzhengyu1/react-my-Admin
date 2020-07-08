1170eafd3aedf528e761804943ee6f825b232563
tzy <634835621>
2020-07-08 19:39:44 +0800

    第三次提交

--- src/pages/Edu/Subject/index.jsx
+++ src/pages/Edu/Subject/index.jsx
@@ -1,11 +1,74 @@
 import React, { Component } from "react";
-import { Button } from 'antd';
+import { Button, Table } from 'antd';
+import { PlusSquareOutlined,FormOutlined,DeleteOutlined } from '@ant-design/icons'
+import './index.less'
+const columns = [
+  { title: '分类名称', dataIndex: 'name', key: 'name' },
+
+  {
+    title: '操作',
+    dataIndex: '',
+    key: 'x',
+    render: () => (
+      <>
+        <Button type='primary' className='update-btn'>
+          <FormOutlined />
+        </Button>
+        <Button type='danger'>
+          <DeleteOutlined />
+        </Button>
+      </>
+    ),
+    width:200
+  },
+];
+const data = [
+  {
+    key: 1,
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+    description: 'My name is John Brown, I am 32 years old, living in New York No. 1 Lake Park.',
+  },
+  {
+    key: 2,
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+    description: 'My name is Jim Green, I am 42 years old, living in London No. 1 Lake Park.',
+  },
+  {
+    key: 3,
+    name: 'Not Expandable',
+    age: 29,
+    address: 'Jiangsu No. 1 Lake Park',
+    description: 'This not expandable',
+  },
+  {
+    key: 4,
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+    description: 'My name is Joe Black, I am 32 years old, living in Sidney No. 1 Lake Park.',
+  },
+];
 export default class Subject extends Component {
   render() {
     return (
-      <>
-       <Button type="primary"></Button>
-      </>
+      <div className='subject'>
+        <Button type="primary" className='subject-btn'>
+          <PlusSquareOutlined />
+         新建
+       </Button>
+        <Table
+          columns={columns}
+          expandable={{
+            expandedRowRender: record => <p style={{ margin: 0 }}>{record.description}</p>,
+            rowExpandable: record => record.name !== 'Not Expandable',
+          }}
+          dataSource={data}
+        />
+      </div>
 
     )
   }
